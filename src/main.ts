07b338b68fcd3c87ee2c24fec82a8b75c1b5f308
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2022-10-15 12:38:16 +0330

    procfile added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new WsAdapter(app));
 
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
